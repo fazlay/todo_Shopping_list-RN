68155ed23804dd15c49cfc52d3600848a4f79636
Fazlay rabbi <rfazlay21@gmail.com>
2022-02-21 14:19:47 +0600

    finaliza

--- src/Index.js
+++ src/Index.js
@@ -1,61 +1,73 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from 'react';
 import {
   Button,
   FlatList,
+  Keyboard,
   SafeAreaView,
+  ScrollView,
   StatusBar,
   Text,
   TextInput,
   TouchableOpacity,
   View,
-} from "react-native";
-import SingleItem from "./SingleItem";
-import { Ionicons } from "@expo/vector-icons";
+} from 'react-native';
+import SingleItem from './SingleItem';
+import { Ionicons } from '@expo/vector-icons';
 
 const Index = () => {
   const [shoppingList, setShoppingList] = useState([
-    { name: "Milk" },
-    { name: "Poweder" },
-    { name: "Ditergent" },
+    { name: 'Milk' },
+    { name: 'Poweder' },
+    { name: 'Ditergent' },
   ]);
   const [singleItem, setSingleItem] = useState([]);
 
   const handleChange = (e) => {
+    console.log(e);
     setSingleItem({ name: e });
   };
-  const handlePress = (e) => {
+  const handlePress = () => {
     // setShoppingList(...shoppingList, setSingleItem);
-    console.log(singleItem);
+    // console.log(singleItem);
+    setShoppingList([...shoppingList, singleItem]);
   };
+
   return (
     <SafeAreaView style={{ marginTop: StatusBar.currentHeight }}>
       <View>
         <Text>This is react Index fiel </Text>
-        <View style={{ flexDirection: "row" }}>
+        <View style={{ flexDirection: 'row' }}>
           <TextInput
+            ref={(input) => {
+              textInput = input;
+            }}
             style={{
               margin: 12,
               borderWidth: 1,
               padding: 10,
-              width: "80%",
+              width: '80%',
             }}
-            onBlur={(e) => console.log(e.target.value)}
+            onChangeText={(e) => handleChange(e)}
+            onBlur={(e) => {}}
             // onChangeText={(e) => {
             //   handleChange(e);
             // }}
-            placeholder="useless placeholder"
-            keyboardType="default"
+            placeholder='useless placeholder'
+            keyboardType='default'
           />
+
           <View>
             <TouchableOpacity
               onPress={() => {
+                Keyboard.dismiss();
                 handlePress();
+                textInput.clear();
               }}
             >
               <Ionicons
-                name="add"
+                name='add'
                 size={24}
-                color="black"
+                color='black'
                 style={{
                   marginTop: 12,
                   marginBottom: 12,
@@ -65,18 +77,6 @@ const Index = () => {
                 }}
               />
             </TouchableOpacity>
-            {/* <Ionicons
-                name="add"
-                size={24}
-                color="black"
-                style={{
-                  marginTop: 12,
-                  marginBottom: 12,
-                  padding: 6,
-                  paddingLeft: 8,
-                  borderWidth: 1,
-                }}
-              /> */}
           </View>
         </View>
 
